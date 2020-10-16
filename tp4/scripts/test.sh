b600b029913662021949c85b87ddf66bacb99dbe
AdrienIT <adrien.zoghbi@ynov.com>
2020-10-16 19:29:12 +0200

    remove useless scripts

--- tp4/scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# AdrienIT
-
-yum install -y yum install zlib-devel libuuid-devel libmnl-devel gcc make git autoconf autogen automake pkgconfig curl jq nodejs wget git
-yum update
-
-bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
